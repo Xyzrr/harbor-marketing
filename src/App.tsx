caa9d5e5796608a1f0b91af56b50bdd2e9234596
John Qian <johnlongqian@gmail.com>
2021-04-04 23:20:52 -0700

    use github release

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ function App() {
     <S.Wrapper>
       <S.GlobalStyles />
       <S.Container>
-        <S.Heading>Give your team some space.</S.Heading>
-        <S.DownloadButton href="./Harbor-2.2.0.dmg.zip" download>
+        <S.Heading>Slogan TBD.</S.Heading>
+        <S.DownloadButton
+          href="https://github.com/Xyzrr/virtual-office/releases/download/0.0.1/Harbor-2.2.0.dmg.zip"
+          download
+        >
           <S.PlatformLogo src={macLogo} />
           Download for MacOS
         </S.DownloadButton>
