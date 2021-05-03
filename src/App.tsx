30df0eff6c58064ad86ae5ac6198822c771dbd67
John Qian <johnlongqian@gmail.com>
2021-05-03 15:41:21 -0700

    just download the latest

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <S.Container>
         <S.Heading>Slogan TBD.</S.Heading>
         <S.DownloadButton
-          href="https://github.com/Xyzrr/virtual-office/releases/download/v0.1.3/Harbor-0.1.3.dmg"
+          href="https://github.com/Xyzrr/virtual-office/releases/latest/Harbor-0.1.6.dmg"
           download
         >
           <S.PlatformLogo src={macLogo} />
