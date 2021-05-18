7801105a5a50c5a0bd00e3c81dc46a45aeedf534
John Qian <johnlongqian@gmail.com>
2021-05-18 11:09:01 -0700

    0.2.17

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <S.Container>
         <S.Heading>Remote work doesn't have to be lonely.</S.Heading>
         <S.DownloadButton
-          href="https://github.com/Xyzrr/harbor/releases/download/v0.2.13/Harbor-0.2.13.dmg"
+          href="https://github.com/Xyzrr/harbor/releases/download/v0.2.17/Harbor-0.2.17.dmg"
           download
         >
           <S.PlatformLogo src={macLogo} />
